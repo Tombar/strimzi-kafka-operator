55fb85ed106269d0fae21ddf95a6363d725b9421
Tom Bentley <tbentley@redhat.com>
2018-08-30 15:42:22 +0100

    kafka sidecar

--- docker-images/kafka-stunnel/scripts/stunnel_config_generator.sh
+++ docker-images/kafka-stunnel/scripts/stunnel_config_generator.sh
@@ -2,6 +2,9 @@
 
 # path were the Secret with certificates is mounted
 CERTS=/etc/tls-sidecar/certs
+# We need to combine all the cluster-ca certs into one
+COMBINED_CA=/tmp/cluster-ca.crt
+cat ${CERTS}/cluster-ca*.crt > "$COMBINED_CA"
 
 CURRENT=${BASE_HOSTNAME}-${KAFKA_BROKER_ID}
 
@@ -12,7 +15,7 @@ echo "debug = info"
 cat <<-EOF
 [zookeeper-2181]
 client = yes
-CAfile = ${CERTS}/cluster-ca.crt
+CAfile = ${COMBINED_CA}
 cert = ${CERTS}/${CURRENT}.crt
 key = ${CERTS}/${CURRENT}.key
 accept = 127.0.0.1:2181
